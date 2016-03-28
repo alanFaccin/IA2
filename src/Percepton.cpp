0940890df578b16bdf40521cbcb74e5e2b371a1b
Alan Jhones Faccin <alan.fts@gmail.com>
2016-03-27 23:33:08 -0300

    Ajuste na funcao de calculo por camada;

--- src/Percepton.cpp
+++ src/Percepton.cpp
@@ -3,38 +3,25 @@
 // Author      : Alan Jhones
 // Version     :
 // Copyright   : Your copyright notice
-// Description :
+// Description :Estruturação de Rede Neural MLP
 //============================================================================
 
 #include <stdio.h>
 //#include <conio.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
+#include  <time.h>
 
 int padroes = 1, funcao = 0, contador = 0, epocas = 0, fim = 0;
 double net = 0, y = 0, erro = 0, ValorDesejado = 0, TaxaAprendizado = 0.005,
-		Entradas[4], DeltaPesos[4], Pesos[4];
-
-
-
+		erro_medio_quadratico = 0;
 
 // Padrıes de entrada da rede (sensores).
-float p[11][2] =
-{
-0.0,    0.0,
-0.0,    1.0,
-1.0,    0.0,
-1.0,    1.0
-};
+float padrao[11][2] = { 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0 };
 
 // Valores desejados dos padrıes ao final do treinamento.
-float d[11][1] =
-{
-0.0,
-1.0,
-1.0,
-0.0
-};
+float d[11][1] = { 0.0, 1.0, 1.0, 0.0 };
 
 const int input = 4; // quantidade de entardas para a camada 1
 const int camada_1 = 2; // quantidade  de nuronios na camada 1(intermediaria)
@@ -81,11 +68,34 @@ double funcaoDeAtivacao(double net, int funcao, double a) {
 	return (valor);
 }
 
-double calcula_net(int qtd_entradas, double *input, double *pesos) {
-	//Calculo do net da saida do neuronio
+double CalculoCamada(int camada, int iteracao) {
 	double somatorio = 0;
-	for (int a = 0; a < qtd_entradas; a++) {
-		somatorio += input[a] * pesos[a];
+	int n = 0;
+	if (camada == 1) {
+		for (int j = 0; j < camada_1; j++) {
+			somatorio = 0;
+			for (int i = 0; i < input; i++) {
+				somatorio += DeltaPesosCamada_1[n] * padrao[iteracao][i];
+				n += 1;
+			}
+			entrada_camada1[j] = somatorio;
+			saida_camada1[j] = funcaoDeAtivacao(entrada_camada1[j], funcao,
+					1.0);
+		}
+
+	} else if (camada == 2) {
+		n = 0;
+		for (int j = 0; j < camada_2; j++) {
+			somatorio = 0;
+			for (int i = 0; i < camada_1; i++) {
+				somatorio += DeltaPesosCamada_2[n] * saida_camada1[i];
+				n += 1;
+			}
+			entrada_camada2[j] = somatorio;
+			saida_camada2[j] = funcaoDeAtivacao(entrada_camada2[j], funcao,
+					1.0);
+		}
+
 	}
 	return somatorio;
 }
@@ -98,24 +108,29 @@ void update_Pesos(int qtd_entradas) {
 	}
 }
 /*função reponsável por realizar o cálculo do erro quadrático, levando em conta a matriz de valor desejado e a saida da camada 2*/
-double calc_erro_quadratico(int k) {
+double calc_erro_quadratico(int iteracao) {
 
 	double err = 0;
-	int j=0;
+	int j = 0;
 	for (j = 0; j < camada_2; j++) {
-		err += pow((d[k][j] - saida_camada2[j]), 2);
+		err += pow((d[iteracao][j] - saida_camada2[j]), 2);
 	}
 	return err;
+}
+double calc_erro_medio_quadratico(double erro_qud, double erro_m_quad) {
+	double err;
+	erro_m_quad += (0.5 * erro_qud);
+	return erro_m_quad;
 }
 /*Funçao responsavel por zerar o array de pesos da camada desejada*/
 void zeraPesos(int camada) {
 	int j = 0;
-	if (camada = 1) {
+	if (camada == 1) {
 		for (j = 0; j < (input * camada_1); j++) {
 			PesosCamada_1[j] = 0.0;
 		}
 
-	} else if (camada = 2) {
+	} else if (camada == 2) {
 		for (j = 0; j < (camada_1 * camada_2); j++) {
 			PesosCamada_2[j] = 0.0;
 		}
@@ -125,14 +140,14 @@ void zeraPesos(int camada) {
 
 void zeraVetores(int camada) {
 	int j = 0;
-	if (camada = 1) {
+	if (camada == 1) {
 		for (j = 0; j < camada_1; j++) {
 			entrada_camada1[j] = 0.0;
 			saida_camada1[j] = 0.0;
 			erro_camada1[j] = 0.0;
 		}
 
-	} else if (camada = 2) {
+	} else if (camada == 2) {
 		for (j = 0; j < camada_2; j++) {
 			entrada_camada2[j] = 0.0;
 			saida_camada2[j] = 0.0;
@@ -144,12 +159,12 @@ void zeraVetores(int camada) {
 
 void randomiza_Pesos(int camada) {
 	int j = 0;
-	if (camada = 1) {
+	if (camada == 1) {
 		for (j = 0; j < (input * camada_1); j++) {
 			PesosCamada_1[j] = (float) (rand() % 100) / 1000;
 		}
 
-	} else if (camada = 2) {
+	} else if (camada == 2) {
 		for (j = 0; j < (camada_1 * camada_2); j++) {
 			PesosCamada_2[j] = (float) (rand() % 100) / 1000;
 		}
@@ -161,14 +176,14 @@ void grava_Pesos_inicials_arquivo(int camada) {
 
 	int j = 0;
 	FILE *fp;
-	fp = fopen("pesos_rand.txt", "wt");
-	if (camada = 1) {
+	fp = fopen("pesos_rand.txt", "a+");
+	if (camada == 1) {
 		fprintf(fp, "Pesos Camada 1\n");
 		for (j = 0; j < (input * camada_1); j++) {
 			fprintf(fp, "%f\n", PesosCamada_1[j]);
 		}
 
-	} else if (camada = 2) {
+	} else if (camada == 2) {
 		fprintf(fp, "Pesos Camada 2\n\n");
 		for (j = 0; j < (camada_1 * camada_2); j++) {
 			fprintf(fp, "%f\n", PesosCamada_2[j]);
@@ -177,13 +192,20 @@ void grava_Pesos_inicials_arquivo(int camada) {
 	fclose(fp);
 }
 
-void grava_erros_treinamento() {
+void treinamento() {
 	FILE *fp;
 	fp = fopen("treinamento.txt", "wt");
-}
-void treinamento() {
+
 	do {
 		contador++;
+		//Laço para a propagação dos padroes pela rede.
+		int k;
+		for (k = 0; k < padroes; k++) {
+			CalculoCamada(1, k);
+			CalculoCamada(2, k);
+			//calc_erro_medio_quadratico(calc_erro_quadratico(k));
+
+		}
 
 	} while (!fim && contador != epocas);
 }
@@ -192,7 +214,6 @@ int main() {
 
 	/* initialize random seed: */
 	srand(time(NULL));
-
 
 	zeraPesos(1);
 	zeraPesos(2);
